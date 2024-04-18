874d18da1b950f03de15b9900962cb5e9823b27d
Dell <goyalkushagra99@gmail.com>
2024-04-19 00:02:45 +0530

    changes in Practice1.java

--- Practice1.java
+++ Practice1.java
@@ -1,5 +1,8 @@
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println();
+        System.out.println("Hello World2");
     }
+
 }
